bda00e3605aa01d96e9697620dc93501aeccd328
Betul Savas <betulsavas457@gmail.com>
2025-04-18 17:52:27 +0300

    proje yüklendi

---
+++ polindrom.py
@@ -0,0 +1,20 @@
+
+
+class Solution(object):
+    def isPalindrome(self, x):
+       
+        rakamlar=[(rakam) for rakam in str (x)]
+
+
+        inverse= list(reversed(rakamlar))
+        print(inverse)
+
+        if rakamlar == inverse:
+            return True
+        
+        else:
+            return False
+
+       
+
+
