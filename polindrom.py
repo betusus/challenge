a016dead6803f45c01a85e7977fe6413b6091a5a
Betül SAVAŞ <betulsavas457@gmail.com>
2025-05-16 23:14:44 +0300

    Update polindrom.py

--- polindrom.py
+++ polindrom.py
@@ -3,7 +3,7 @@
 class Solution(object):
     def isPalindrome(self, x):
        
-        rakamlar=[(rakam) for rakam in str (x)]
+        rakamlar=[(rakam) for rakam in str (x)];
 
 
         inverse= list(reversed(rakamlar))
